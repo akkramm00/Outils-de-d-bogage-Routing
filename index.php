81d5b0aee0c2d6b7e85e7a0f6f06d4062172ccb5
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-22 15:45:17 +0000

    add essentiel "conclusion

--- index.php
+++ index.php
@@ -15,6 +15,24 @@
 
     grace à la commande "debug:router" nous pourrons visualiser toutes les routesde l'application dans un tableau.
 
-    Ici, il y a 3 routes dans le cadre d'un environnement en développement
+    Ici, il y a 3 routes dans le cadre d'un environnement en développement.
+    Nous verrons plus de routes ,notamment celles liées au débogage.
+
+
+    <h2>Essentiel</h2>
+
+    <p>
+      Le routing ou routage est un élément clé des applications web modernes. il permet de définir comment les URL de votre application doivent être mappées aux actions. 
+      
+      Symfony permet de configurer simplement le routing à l'aide d'un système d'annotations ou de PHP attributes, d'un fichier tiers en YAML ou plus rarement en XML voire en PHP.
+      
+      De plus , nous avnos vu comment relier le routage a nos controllers. Nous avons également vu qu'il exixteplusieurs paramètres de route et nous avons étudieé les p^lus utilisés : name, path et son paramètre, methods, requirements, schemes, defaults.
+      
+      Méme s'il est possible de faire cohabiter p^lusieurs méthodes de routing, il est de rigueur pour nous et pour ceux qui reprendront éventuellement le code, de choisir une seule méthode, àsavoir un système d'annotations qui permetde visualiser immédiatement le routing avec la méthode du controller.
+
+      Cependant, les fichiers YAML sont une bonne alternative, même s'ils nécessitent plus de manipulation dans notre code. Pensez à bien organiser vos fichiers si vous travaillez ou opter pouir cette solutionde routing, ainsi qu'à utiliser les outils de débogage qui vous feront gagner un temps précieux.
+
+      
+    </p>
   </body>
 </html>
