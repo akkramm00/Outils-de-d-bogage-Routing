0fee378f761e5ad9e466e2a9e242a6cea1bf8b3a
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-22 15:23:38 +0000

    le débogage avec la commande "debug:router

--- index.php
+++ index.php
@@ -4,5 +4,17 @@
   </head>
   <body>
   <h1>Outils de débogage </h1>
+    <p>
+      Les outils de débogage pour symfony sont des fonctionnalités intégrés dans le framework qui permettent aux développeurs de diagnostiquer et de résoudre les erreurs et les problèmes dans lleur applicationSymfony. Dans cette partie du cours,nous allons nous en imprégner en commençant par présenter brièvement la commande "debug:router" .
+
+      La commande "debug:router" permet de connaitre toutes les routes qui sont dans l'application .
+    </p>
+    <h2>Méthode</h2>
+
+    $ symfony console debug:router
+
+    grace à la commande "debug:router" nous pourrons visualiser toutes les routesde l'application dans un tableau.
+
+    Ici, il y a 3 routes dans le cadre d'un environnement en développement
   </body>
 </html>
